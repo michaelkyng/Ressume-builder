7083f41c22d4ac768d2b65f1faa8cf1ef763b6d5
Michael Akpuofoba <67930856+michaelkyng@users.noreply.github.com>
2022-11-23 08:01:52 +0100

    Improvements

---
+++ js/Bootstrap js/builder.js
@@ -0,0 +1,6 @@
+function replace_template(){
+    let template_replace = document.getElementById("template")
+    template_replace = template
+    console.log(template_replace)
+    console.log("Template replaced")
+}
---
+++ js/Bootstrap js/choosetemplate.js
@@ -0,0 +1,69 @@
+function change_color_black(){
+let rect_color = document.getElementById("side-bar")
+let rect_color2 = document.getElementById("side-bar2")
+rect_color.style.fill = "black"
+rect_color2.style.fill = "black"
+console.log("color changed to black")
+}
+
+function change_color_lightblue(){
+    let rect_color = document.getElementById("side-bar")
+    let rect_color2 = document.getElementById("side-bar2")
+    rect_color.style.fill = "#2487f1"
+    rect_color2.style.fill = "#2487f1"
+    console.log("color changed to light blue")
+}
+    
+function change_color_darkblue(){
+    let rect_color = document.getElementById("side-bar")
+    let rect_color2 = document.getElementById("side-bar2")
+    rect_color.style.fill = "#003194"
+    rect_color2.style.fill = "#003194"
+    console.log("color changed to dark blue")
+}
+    
+function change_color_red(){
+    let rect_color = document.getElementById("side-bar")
+    let rect_color2 = document.getElementById("side-bar2")
+    rect_color.style.fill = "red"
+    rect_color2.style.fill = "red"
+    console.log("color changed to red")
+}
+
+function change_color_green(){
+    let rect_color = document.getElementById("side-bar")
+    let rect_color2 = document.getElementById("side-bar2")
+    rect_color.style.fill = "green"
+    rect_color2.style.fill = "green"
+    console.log("color changed to green")
+}
+
+function change_color_yellow(){
+    let rect_color = document.getElementById("side-bar")
+    let rect_color2 = document.getElementById("side-bar2")
+    rect_color.ch = "yellow"
+    rect_color2.style.fill = "yellow"
+    console.log("color changed to yellow")
+}
+    
+function use_template(){
+    temp_template = document.getElementsByClassName("active")
+    fake_template = temp_template[1]
+    temp = fake_template.firstElementChild
+    template1 = temp.firstElementChild
+    console.log(template1)
+    var content = "What's up , hello world";
+    // any kind of extension (.txt,.cpp,.cs,.bat)
+    var filename = "hello.txt";
+
+    var blob = new Blob([content], {
+    type: "text/plain;charset=utf-8"
+        });
+
+    saveAs(blob, filename);
+
+    let move_to_builder = document.getElementById("template-btn")
+    move_to_builder.href = "builder.html"
+    
+}
+
