066c7cb1c910e0ebf580b028e6e78eede4162793
Kyng Cytro <johndibashi2001@gamil.com>
2022-11-25 18:44:37 +0100

    :bug: bug fixes & improvements

--- js/Bootstrap js/builder.js
+++ js/Bootstrap js/builder.js
@@ -1,6 +1,17 @@
 function generateCV() {
   window.event.preventDefault();
 
+  // Capitalize
+
+  const capitalize = (word) => {
+    return word[0].toUpperCase() + word.substring(1);
+  };
+
+  const getParameter = (key) => {
+    let address = window.location.search;
+    let parameterList = new URLSearchParams(address);
+    return parameterList.get(key);
+  };
   //Get Data from form
 
   // Contact info
@@ -18,7 +29,6 @@ function generateCV() {
   const employer = document.querySelector("#employer").value;
   const job_city = document.querySelector("#job-city").value;
   const job_state = document.querySelector("#job-state").value;
-  const job_country = document.querySelector("#job-country").value;
   const job_start_date = document.querySelector("#job-start-date").value;
   const job_end_date = document.querySelector("#job-end-date").value;
 
@@ -29,36 +39,165 @@ function generateCV() {
   const school_country = document.querySelector("#school-country").value;
   const school_start_date = document.querySelector("#school-start-date").value;
   const school_end_date = document.querySelector("#school-end-date").value;
-
+  const degree = document.querySelector("#school-degree").value;
   //Skill
-  const skills = document.querySelector("#skills").value;
+  //const skills = document.querySelector("#skills").value;
 
+  const prof_summary = document.querySelector("#prof-summary").value;
   // Convert to object
-  const data = {
-    name,
-    email,
-    phone,
-    address,
-    city,
-    state,
-    postal_code,
-    country,
-    job_title,
-    employer,
-    job_city,
-    job_state,
-    job_country,
-    job_start_date,
-    job_end_date,
-    school,
-    school_city,
-    school_state,
-    school_country,
-    school_start_date,
-    school_end_date,
-    skills,
-  };
-
-  var newWindow = window.open();
-  newWindow.document.location.href = "template.html";
+  var win = window.open(
+    "",
+    "Title",
+    "toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=1024,height=2000,top=" +
+      (screen.height - 400) +
+      ",left=" +
+      (screen.width - 840)
+  );
+  win.document.body.innerHTML = `<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Name</title>
+      <style>
+        * {
+          margin: 0;
+          padding: 0;
+        }
+        .containar {
+          margin-left: auto;
+          margin-right: auto;
+          max-width: 800px;
+          height: 100vh;
+          display: flex;
+          gap: 10px;
+          justify-content: center;
+        }
+        .containar .left {
+          width: 70%;
+        }
+        .containar .right {
+          width: 30%;
+          background-color: #${getParameter("color")};
+        }
+        .name-box {
+          background-color: #${getParameter("color")};
+          height: 23%;
+          display: flex;
+          flex-direction: column;
+          justify-content: flex-end;
+          padding-left: 10px;
+          color: white;
+        }
+        .name-box p {
+          font-size: 40px;
+          text-transform: uppercase;
+        }
+        .info-box-right {
+          padding-left: 10px;
+          padding-right: 10px;
+        }
+        .info-box-left {
+          padding-left: 10px;
+        }
+        .header-box {
+          margin-top: 30px;
+          background-color: gainsboro;
+          padding: 10px;
+        }
+        .header-box p {
+          font-size: 12px;
+          text-transform: uppercase;
+          font-weight: bold;
+        }
+        .summary-box .text-box {
+          padding-left: 10px;
+          padding-top: 15px;
+        }
+        .info-box-right .text-box {
+          color: white;
+        }
+        .info-box-right ul {
+          margin-left: 20px;
+        }
+        .contact {
+          margin-top: 190px;
+        }
+        .skills {
+          padding-top: 50px;
+        }
+      </style>
+    </head>
+    <body>
+      <div class="containar">
+        <div class="content left">
+          <div class="name-box">
+            <p>${name.split(" ")[0]}</p>
+            <p>${name.split(" ")[1]}</p>
+          </div>
+          <div class="info-box-left">
+            <div class="summary-box prof-summary">
+              <div class="header-box">
+                <p>Professional Summary</p>
+              </div>
+              <p class="text-box">
+                  ${prof_summary}
+              </p>
+            </div>
+            <div class="summary-box work-summary">
+              <div class="header-box">
+                <p>Work Experience</p>
+              </div>
+              <p class="text-box">
+                ${capitalize(employer)}, ${capitalize(job_city)}, ${capitalize(
+    job_state
+  )} from (${job_start_date} to ${job_end_date}) -
+                ${capitalize(job_title)}
+              </p>
+            </div>
+            <div class="summary-box work-summary">
+              <div class="header-box">
+                <p>Education</p>
+              </div>
+              <p class="text-box">from (${school_start_date}) to (${school_end_date})</p>
+              <p class="text-box">${capitalize(degree)}</p>
+              <p class="text-box">
+                ${capitalize(school)}, ${capitalize(school_city)}, ${capitalize(
+    school_state
+  )}, ${capitalize(school_country)}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="content right">
+          <div class="info-box-right">
+            <div class="summary-box contact">
+              <div class="header-box">
+                <p>Contact</p>
+              </div>
+              <p class="text-box">${address}, ${capitalize(city)}, ${capitalize(
+    state
+  )}, ${country} ${postal_code}</p>
+              <p class="text-box">${phone}</p>
+              <p class="text-box">${email}</p>
+            </div>
+            <div class="summary-box skills">
+              <div class="header-box">
+                <p>Skills</p>
+              </div>
+              <ul class="text-box">
+                <li>TRAINED IN POINT-OF-SALE SYSTEM</li>
+                <li>SPANISH</li>
+                <li>GIT</li>
+                <li>PYTHON</li>
+                <li>JAVASCRIPT</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </body>
+  </html>
+  `;
 }
--- js/Bootstrap js/choosetemplate.js
+++ js/Bootstrap js/choosetemplate.js
@@ -1,59 +1,65 @@
-function change_color_black(){
-let rect_color = document.getElementById("side-bar")
-let rect_color2 = document.getElementById("side-bar2")
-rect_color.style.fill = "black"
-rect_color2.style.fill = "black"
-console.log("color changed to black")
+let color = "00aa93";
+
+function change_color_black() {
+  let rect_color = document.getElementById("side-bar");
+  let rect_color2 = document.getElementById("side-bar2");
+  rect_color.style.fill = "black";
+  rect_color2.style.fill = "black";
+  color = "000000";
+  console.log("color changed to black");
 }
 
-function change_color_lightblue(){
-    let rect_color = document.getElementById("side-bar")
-    let rect_color2 = document.getElementById("side-bar2")
-    rect_color.style.fill = "#2487f1"
-    rect_color2.style.fill = "#2487f1"
-    console.log("color changed to light blue")
-}
-    
-function change_color_darkblue(){
-    let rect_color = document.getElementById("side-bar")
-    let rect_color2 = document.getElementById("side-bar2")
-    rect_color.style.fill = "#003194"
-    rect_color2.style.fill = "#003194"
-    console.log("color changed to dark blue")
-}
-    
-function change_color_red(){
-    let rect_color = document.getElementById("side-bar")
-    let rect_color2 = document.getElementById("side-bar2")
-    rect_color.style.fill = "red"
-    rect_color2.style.fill = "red"
-    console.log("color changed to red")
+function change_color_lightblue() {
+  let rect_color = document.getElementById("side-bar");
+  let rect_color2 = document.getElementById("side-bar2");
+  rect_color.style.fill = "#2487f1";
+  rect_color2.style.fill = "#2487f1";
+  color = "2487f1";
+  console.log("color changed to light blue");
 }
 
-function change_color_green(){
-    let rect_color = document.getElementById("side-bar")
-    let rect_color2 = document.getElementById("side-bar2")
-    rect_color.style.fill = "green"
-    rect_color2.style.fill = "green"
-    console.log("color changed to green")
+function change_color_darkblue() {
+  let rect_color = document.getElementById("side-bar");
+  let rect_color2 = document.getElementById("side-bar2");
+  rect_color.style.fill = "#003194";
+  rect_color2.style.fill = "#003194";
+  color = "#003194";
+  console.log("color changed to dark blue");
 }
 
-function change_color_yellow(){
-    let rect_color = document.getElementById("side-bar")
-    let rect_color2 = document.getElementById("side-bar2")
-    rect_color.style.fill = "yellow"
-    rect_color2.style.fill = "yellow"
-    console.log("color changed to yellow")
-}
-    
-function use_template(){
-    temp_template = document.getElementsByClassName("active")
-    fake_template = temp_template[1]
-    temp = fake_template.firstElementChild
-    template1 = temp.firstElementChild
-    console.log(template1)
-    let move_to_builder = document.getElementById("template-btn")
-    move_to_builder.href = "builder.html"
-    
+function change_color_red() {
+  let rect_color = document.getElementById("side-bar");
+  let rect_color2 = document.getElementById("side-bar2");
+  rect_color.style.fill = "red";
+  rect_color2.style.fill = "red";
+  color = "ff0000";
+  console.log("color changed to red");
 }
 
+function change_color_green() {
+  let rect_color = document.getElementById("side-bar");
+  let rect_color2 = document.getElementById("side-bar2");
+  rect_color.style.fill = "green";
+  rect_color2.style.fill = "green";
+  color = "00ff00";
+  console.log("color changed to green");
+}
+
+function change_color_yellow() {
+  let rect_color = document.getElementById("side-bar");
+  let rect_color2 = document.getElementById("side-bar2");
+  rect_color.style.fill = "yellow";
+  rect_color2.style.fill = "yellow";
+  color = "ffff00";
+  console.log("color changed to yellow");
+}
+
+function use_template() {
+  temp_template = document.getElementsByClassName("active");
+  fake_template = temp_template[1];
+  temp = fake_template.firstElementChild;
+  template1 = temp.firstElementChild;
+  console.log(template1);
+  let move_to_builder = document.getElementById("template-btn");
+  move_to_builder.href = `builder.html?color=${color}`;
+}
